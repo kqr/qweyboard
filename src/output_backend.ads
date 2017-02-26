9c00f452281be0ddee7cea1309f204143eae605d
kqr <k@rdw.se>
2017-02-26 05:18:24 +0100

    tiny cleanup

--- src/output_backend.ads
+++ src/output_backend.ads
@@ -1,12 +1,10 @@
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Logging;
 
--- TODO: Universify this by putting all the C/X11 stuff in the body!
 package Output_Backend is
    use Logging;
 
    task Output is
       entry Enter (Text : Unbounded_String);
    end Output;
-private
 end Output_Backend;
